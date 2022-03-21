52a123787e48c7233666dba378837c6902f36264
marcoswlrich <marcosapw.dev@gmail.com>
2022-03-20 22:58:47 -0300

    Rolagem

--- js/script.js
+++ js/script.js
@@ -15,7 +15,49 @@ btnNavEl.addEventListener('click', function () {
 })
 
 // Animação de rolagem mais suave
+const allLinks = document.querySelectorAll('a:link')
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault()
+    const href = link.getAttribute('href')
+
+    // Rolar de volta ao topo
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      })
+
+    // Rolagem para outros links
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href)
+      sectionEl.scrollIntoView({ behavior: 'smooth' })
+    }
+
+    // Fechar mobile navigation
+    if (link.classList.contains('main-nav-link'))
+      headerEl.classList.toggle('nav-open')
+  })
+})
 
 // Navegação fixa
 
 // Correção da propriedade flexbox gap ausente em algumas versões do Safari
+function checkFlexGap() {
+  var flex = document.createElement('div')
+  flex.style.display = 'flex'
+  flex.style.flexDirection = 'column'
+  flex.style.rowGap = '1px'
+
+  flex.appendChild(document.createElement('div'))
+  flex.appendChild(document.createElement('div'))
+
+  document.body.appendChild(flex)
+  var isSupported = flex.scrollHeight === 1
+  flex.parentNode.removeChild(flex)
+  console.log(isSupported)
+
+  if (!isSupported) document.body.classList.add('no-flexbox-gap')
+}
+checkFlexGap()
