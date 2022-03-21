55f23ca71c346de637caa4fe2edb124d4419d269
marcoswlrich <marcosapw.dev@gmail.com>
2022-03-20 22:24:17 -0300

    Scripts do site - Definição de ano

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const myName = 'Marcos Wlrich'
+const h1 = document.querySelector('.heading-primary')
+
+// Definir ano atual
+const yearEl = document.querySelector('.year')
+const currentYear = new Date().getFullYear()
+yearEl.textContent = currentYear
+
+// Funcionamento da navegação móvel
+
+// Animação de rolagem mais suave
+
+// Navegação fixa
+
+// Correção da propriedade flexbox gap ausente em algumas versões do Safari
